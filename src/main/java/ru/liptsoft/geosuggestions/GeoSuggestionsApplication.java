6f415557b803b008ad6da4e5447a143936e20f30
m.krasilnikov <wiezmin92@mail.ru>
2019-05-20 23:39:50 +0300

    Fix war packaging

--- src/main/java/ru/liptsoft/geosuggestions/GeoSuggestionsApplication.java
+++ src/main/java/ru/liptsoft/geosuggestions/GeoSuggestionsApplication.java
@@ -6,11 +6,13 @@ import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 @EnableProcessApplication
-public class GeoSuggestionsApplication {
+public class GeoSuggestionsApplication extends SpringBootServletInitializer {
 
 	@Autowired
 	private RuntimeService runtimeService;
@@ -18,6 +20,13 @@ public class GeoSuggestionsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GeoSuggestionsApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
+	}
+
+	private static Class<GeoSuggestionsApplication> applicationClass = GeoSuggestionsApplication.class;
 
 	@EventListener
 	private void processPostDeploy(PostDeployEvent event) {
